fb29764eddf24107a706e7aad6b6b4630a7d4504
vu2ptt <72292002+vu2ptt@users.noreply.github.com>
2024-10-29 13:06:37 +0530

    Updates
    write-c2-files.cfg was not being updated - now fixed

--- deinstall.sh
+++ deinstall.sh
@@ -6,6 +6,8 @@ $DIR/stop.sh
 mount -o rw,remount /media/mmcblk0p1
 cp $DIR/decode-ft8.sh.orig $DIR/decode-ft8.sh
 rm $DIR/decode-ft8.sh.orig
+cp $DIR/write-c2-files.cfg.orig $DIR/write-c2-files.cfg
+rm $DIR/write-c2-files.cfg.orig
 lbu commit -d
 mount -o ro,remount /media/mmcblk0p1
 $DIR/start.sh
--- install.sh
+++ install.sh
@@ -16,6 +16,10 @@ cp upload-to-rbn $DIR
 test -e $DIR/decode-ft8.sh.orig || cp $DIR/decode-ft8.sh $DIR/decode-ft8.sh.orig
 cp decode-ft8.sh $DIR
 
+# Back up original write-c2-files.cfg 
+test -e $DIR/write-c2-files.cfg.orig || cp $DIR/write-c2-files.cfg $DIR/write-c2-files.cfg.orig
+cp write-c2-files.cfg $DIR
+
 # Set FT8 receiver to run at boot
 cp $DIR/start.sh /media/mmcblk0p1
 
